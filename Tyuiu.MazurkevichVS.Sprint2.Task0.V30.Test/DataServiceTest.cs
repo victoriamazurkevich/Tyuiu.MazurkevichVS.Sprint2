3eface01af21eb85c22383964cd31fff0fff4471
Виктория Мазуркевич <victoria090707maz@gmail.com>
2025-10-13 19:21:28 +0500

    Add project files.

---
+++ Tyuiu.MazurkevichVS.Sprint2.Task0.V30.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.MazurkevichVS.Sprint2.Task0.V30.Lib;
+namespace Tyuiu.MazurkevichVS.Sprint2.Task0.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 95;
+            int y = 1735;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            bool[] expected = { true, false, true, false, true, false };
+            CollectionAssert.AreEqual(expected, res);
+            
+        }
+    }
+}
---
+++ Tyuiu.MazurkevichVS.Sprint2.Task0.V30/Program.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.MazurkevichVS.Sprint2.Task0.V30.Lib;
+
+DataService ds = new DataService();
+int x = 95;
+int y = 1735;
+bool[] res = new bool[6];
+res = ds.GetCompareOperations(x, y);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("X = " + x);
+Console.WriteLine("Y = " + y);
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+for (int i = 0; i < 6; i++)
+{
+    Console.WriteLine(res[i]);
+}
+Console.ReadKey();
