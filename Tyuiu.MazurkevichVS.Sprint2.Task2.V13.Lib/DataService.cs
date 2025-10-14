dc5caee295f2f763935a86e4d390c1bb402fe59b
Виктория Мазуркевич <victoria090707maz@gmail.com>
2025-10-14 19:39:04 +0500

    Task2

---
+++ Tyuiu.MazurkevichVS.Sprint2.Task2.V13.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Design;
+using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.MazurkevichVS.Sprint2.Task2.V13.Lib
+{
+    public class DataService : ISprint2Task2V13
+    {
+        public bool CheckDotInShadedArea(int x, int y)
+        {
+            bool res;
+            if ((x >= 3 && x<= 13 && y == 6) || (x == 4 && y >= 2 && y <= 8) || (x >= 8 && x <= 12 && y >= 5 && y <= 10) || (x >= 3 && x <= 9 && y == 11) || (x >= 6 && x <= 7 && y == 10) || (x >= 7 && x <= 10 && y == 11) || (x >= 9 && x <= 12 && y >= 3 && y <= 4) || (x == 13 && y >= 6 && y <= 8))
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.MazurkevichVS.Sprint2.Task2.V13.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.MazurkevichVS.Sprint2.Task2.V13.Lib;
+namespace Tyuiu.MazurkevichVS.Sprint2.Task2.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 4;
+            int y = 6;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool expected = true;
+            Assert.AreEqual(expected, res);
+        }
+    }
+}
