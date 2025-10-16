9c9a90a62bc9874c3c2fb6e49b23c1689d6174f6
Виктория Мазуркевич <victoria090707maz@gmail.com>
2025-10-16 15:42:54 +0500

    Task7

--- Tyuiu.MazurkevichVS.Sprint2.Task7.V14.Lib/DataService.cs
+++ Tyuiu.MazurkevichVS.Sprint2.Task7.V14.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.MazurkevichVS.Sprint2.Task7.V14.Lib
         public bool CheckDotInShadedArea(double x, double y)
         {
             bool res;
-            if (((Math.Pow(x, 2) + Math.Pow(y, 2)) <= 1) && ((y <= -x)) && (Math.Abs((x-1))>=1))
+            if (((Math.Pow(x, 2) + Math.Pow(y, 2)) <= 1) && ((y >= -x)) && (Math.Abs((x-1))<=0))
             {
                 res = true;
             }
