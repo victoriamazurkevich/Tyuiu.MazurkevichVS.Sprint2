022ed5f3c9b181b5851364def88836790d211c93
Виктория Мазуркевич <victoria090707maz@gmail.com>
2025-10-16 12:44:04 +0500

    Task4

---
+++ Tyuiu.MazurkevichVS.Sprint2.Task4.V20.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.MazurkevichVS.Sprint2.Task4.V20.Lib;
+namespace Tyuiu.MazurkevichVS.Sprint2.Task4.V20.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 1;
+            double z = ds.Calculate(x, y);
+            double expected = 8;
+            Assert.AreEqual(expected, z);
+        }
+    }
+}
---
+++ Tyuiu.MazurkevichVS.Sprint2.Task4.V20/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.MazurkevichVS.Sprint2.Task4.V20.Lib;
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Введите значение переменной X:");
+double x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение переменной Y:");
+double y = Convert.ToInt32(Console.ReadLine());
+
+DataService ds = new DataService();
+double z = ds.Calculate(x, y);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Z = " + z);
+
+Console.ReadKey();
